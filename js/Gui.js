053e1a325c7bcd37452f0a46b5717b60d6d58f40
lo-th <3dflashlo@gmail.com>
2016-04-02 11:09:01 +0200

    up

--- js/Gui.js
+++ js/Gui.js
@@ -17,27 +17,46 @@ var Gui = ( function () {
         //UIL.COLOR = 'no';
         ui = new UIL.Gui({size:250, color:'no'});
 
+        this.topMenu();
         this.basicMenu();
 
     };
 
-    Gui.basicMenu = function(){
+    Gui.resize = function(){
 
-        ui.add('button', { name:'Man/Woman', p:4, h:40 } ).onChange( function(v){ switchGender(); } );
+    };
+
+    Gui.topMenu = function(){
+
+        var c = document.createElement('canvas');
+
+
+
+
+    };
+
+    Gui.basicMenu = function(){
+        ui.add('title', { name:'AVATAR LAB', prefix:'v0.1', h:30, r:10 } );//.onChange( function(v){ switchGender(); } );
+        ui.add('button', { name:'Man/Woman', p:4, h:30, r:10 } ).onChange( function(v){ switchGender(); } );
         ui.add('bool', { name:'Visible', p:70, h:20, value:true } ).onChange( function(v){ heroVisibility(); } );
         ui.add('bool', { name:'BVH Skeleton', p:70, h:20, value:false } ).onChange( function(v){ skeletonVisibility(); } );
         ui.add('bool', { name:'Helper', p:70, h:20, value:false } ).onChange( function(v){ helperVisibility(); } );
-        ui.add('bool', { name:'PostEffect', p:70, h:20, value:false } ).onChange( function(v){ postEffect(); } );
-        ui.add('bool', { name:'Shadow', p:70, h:20, value:false } ).onChange( function(v){ shadow(); } );
+
+        var gr0 = ui.add('group', { name:'Render Options', height:30 });
+
+        gr0.add('bool', { name:'PostEffect', p:70, h:20, value:false } ).onChange( function(v){ postEffect(); } );
+        gr0.add('bool', { name:'Shadow', p:70, h:20, value:false } ).onChange( function(v){ shadow(); } );
     
 
-        ui.add('list',   { name:'list',  list:['None', 'Linear', 'Reinhard', 'Cineon', 'Uncharted2'], height:30, value:'Uncharted2'}).onChange( function(v){ setToneMap(v);  } );
-        ui.add('slide',  { name:'Exposure',  min:0, max:10, value:3, precision:1, fontColor:'#D4B87B' }).onChange( function(v){ renderer.toneMappingExposure = v; } );
-        ui.add('slide',  { name:'WhiteP',  min:0, max:10, value:5, precision:1, fontColor:'#D4B87B' }).onChange( function(v){ renderer.toneMappingWhitePoint = v; } );
+        gr0.add('list',   { name:'ToneMap',  list:['None', 'Linear', 'Reinhard', 'Cineon', 'Uncharted2'], height:30, value:'Uncharted2'}).onChange( function(v){ setToneMap(v);  } );
+        gr0.add('slide',  { name:'Exposure',  min:0, max:10, value:3, precision:1, fontColor:'#D4B87B' }).onChange( function(v){ renderer.toneMappingExposure = v; } );
+        gr0.add('slide',  { name:'WhiteP',  min:0, max:10, value:5, precision:1, fontColor:'#D4B87B' }).onChange( function(v){ renderer.toneMappingWhitePoint = v; } );
 
-        ui.setBG('rgba(48,48,48,0.8)')
+        gr0.open();
 
-    }
+        ui.setBG('rgba(128,128,128,0.8)')
+
+    };
 
 
 
